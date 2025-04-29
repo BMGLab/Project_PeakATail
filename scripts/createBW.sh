0b7e0eebeec312667c8d9fc39df079ec35747586
ebrukocakaya <kocakayaebru@gmail.com>
2025-04-29 15:26:09 +0300

    scripts

--- scripts/createBW.sh
+++ scripts/createBW.sh
@@ -3,10 +3,30 @@
 inputbam=$1
 outprefix=$2
 
-samtools $inputbam
+# Check if arguments are provided
+if [ -z "$inputbam" ] || [ -z "$outprefix" ]; then
+  echo "Usage: $0 <input.bam> <output_prefix>"
+  exit 1
+fi
 
+# Check if BAM index exists
+if [ ! -f "${inputbam}.bai" ]; then
+  echo "BAM index not found. Creating index..."
+  samtools index "$inputbam"
+fi
+
+
+# Example samtools command (optional, remove if not needed)
+# Just to check the BAM file, you might want something like:
+samtools quickcheck "$inputbam"
+if [ $? -ne 0 ]; then
+  echo "samtools quickcheck failed. BAM file might be corrupt."
+  exit 1
+fi
+
+# Activate conda environment properly
+source $(conda info --base)/etc/profile.d/conda.sh
 conda activate deeptools
 
-bamCoverage -b $inputbam -o "${outfile}".bw --binSize 100 --normalizeUsing CPM
-
-
+# Run bamCoverage
+bamCoverage -b "$inputbam" -o "${outprefix}.bw" --binSize 100 --normalizeUsing CPM
